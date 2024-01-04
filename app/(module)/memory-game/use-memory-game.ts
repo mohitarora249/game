f12cc4bbc3b10db0aa760dd67e5b9a4f3106982b
mohitarora249 <mohitarora249@gmail.com>
2024-01-04 08:18:46 +0000

    WIP: memory game -> game end logic working

--- app/(module)/memory-game/use-memory-game.ts
+++ app/(module)/memory-game/use-memory-game.ts
@@ -5,7 +5,8 @@ const CARDS = ["&#x1F60A;", "&#x1F604;", "&#x1F600;", "&#x1F603;", "&#x1F601;", 
 const useMemoryGame = () => {
     const [game, setGame] = useState<string[]>([]);
     const [gameState, setGameState] = useState<"new" | "inprogress" | "tied" | "over">("inprogress");
-
+    const [filppedCardsIdx, setFilppedCardsIdx] = useState<number[]>([]);
+    
     useEffect(() => {
         shuffleCards();
     }, [])
@@ -18,12 +19,34 @@ const useMemoryGame = () => {
     const startOver = () => {
         setGameState("inprogress");
         shuffleCards();
+        setFilppedCardsIdx([]);
     };
+
+    const isGameOver = (cards: string[]) => cards.every(g => g === "");
+
+    const onCardClickHandler = (idx: number) => {
+        if (gameState === "over") return;
+        if (filppedCardsIdx.length === 0) setFilppedCardsIdx([idx]);
+        else if(filppedCardsIdx.length === 1) {
+            setFilppedCardsIdx([...filppedCardsIdx, idx]);
+            if (game[filppedCardsIdx[0]] === game[idx]) {
+                const cards = [...game];
+                cards[filppedCardsIdx[0]] = "";
+                cards[idx] = "";
+                setTimeout(() => { setGame(cards) }, 600);
+                if (isGameOver(cards))  setGameState("over");
+            } else {
+                setTimeout(() => { setFilppedCardsIdx([]) }, 600);
+            }
+        } else setFilppedCardsIdx([idx]);
+    }
 
     return {
         game,
         gameState,
-        startOver
+        startOver,
+        filppedCardsIdx,
+        onCardClickHandler
     }
 };
 
--- app/memory-game/page.tsx
+++ app/memory-game/page.tsx
@@ -2,20 +2,40 @@
 import useMemoryGame from "../(module)/memory-game/use-memory-game";
 
 const MemoryGame = () => {
-  const { game, gameState } = useMemoryGame();
-  
+  const { game, gameState, onCardClickHandler, filppedCardsIdx, startOver } = useMemoryGame();
   return (
     <div className="h-screen w-screen flex items-center justify-center flex-col">
       <h1 className="text-3xl mb-8">Memory Game</h1>
       <div className="grid grid-cols-4">
-          {game.map(g => 
-            <div key={g} className="h-32 w-32 border border-gray-300 flex justify-center items-center">
-              <div className="text-[6rem] flex justify-center items-center" dangerouslySetInnerHTML={{__html: g }} />
-            </div>
-          )}
+        {game.map((g, i) =>
+          <Cell filppedCardsIdx={filppedCardsIdx} emoji={g} idx={i} onCardClickHandler={onCardClickHandler} />
+        )}
       </div>
+      <p className="text-2xl my-4 flex flex-col space-y-4">
+        <button onClick={startOver}>Restart</button>
+        {gameState === "over" && <span>You won!</span>}
+      </p>
     </div>
   )
 };
+
+type Props = {
+  idx: number;
+  emoji: string;
+  filppedCardsIdx: number[];
+  onCardClickHandler: (idx: number) => void;
+}
+
+const Cell = ({ idx, onCardClickHandler, emoji, filppedCardsIdx }: Props) => {
+  const onClick = () => {
+    onCardClickHandler(idx)
+  }
+  if(emoji === "") return <div className="h-32 w-32 border border-gray-300 flex justify-center items-center" />
+  return (
+    <div onClick={onClick} key={emoji} className="h-32 w-32 border border-gray-300 flex justify-center items-center">
+      {filppedCardsIdx.indexOf(idx) > -1 ? <div className="text-[6rem] flex justify-center items-center" dangerouslySetInnerHTML={{ __html: emoji }} /> : <div className="text-[6rem] flex justify-center items-center">Flip</div>}
+    </div>
+  )
+}
 
 export default MemoryGame;
